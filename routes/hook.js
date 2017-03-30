a746cdb2f06414caeda283278bc2faa58d886321
Greg Allen <greg@firstandthird.com>
2017-03-30 15:18:55 -0700

    initial commit

---
+++ routes/hook.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+const Boom = require('boom');
+const runshell = require('runshell');
+exports.hook = {
+  path: '/',
+  method: 'POST',
+  config: {
+    validate: {
+      query: {
+        secret: Joi.string()
+      },
+      payload: {
+        user: Joi.string(),
+        repo: Joi.string(),
+        branch: Joi.string(),
+        image: Joi.string()
+      }
+    }
+  },
+  handler(request, reply) {
+    const config = request.server.settings.app;
+    const secret = config.secret;
+    if (secret !== request.query.secret) {
+      return reply(Boom.unauthorized());
+    }
+
+    reply('ok');
+
+    const payload = request.payload;
+    runshell(config.deployScript, {
+      env: {
+        GITHUB_USER: payload.user,
+        GITHUB_REPO: payload.repo,
+        GITHUB_BRANCH: payload.branch,
+        DOCKER_IMAGE: payload.image
+      },
+      log: true
+    });
+  }
+};
+
