e111db6337564bf8a83c83a9b619510109723274
Greg Allen <greg@firstandthird.com>
2017-08-09 10:36:55 -0700

    allow passing auth via env var

--- methods/docker/authConfig.js
+++ methods/docker/authConfig.js
@@ -2,11 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 module.exports = function(done) {
-  const dockerConfig = path.join(os.homedir(), '.docker/config.json');
-  if (fs.existsSync(dockerConfig)) {
-    const config = require(dockerConfig);
-    const addr = 'https://index.docker.io/v1/';
-    const auth = config.auths[addr].auth;
+  const server = this;
+  const addr = 'https://index.docker.io/v1/';
+  let auth = server.settings.app.dockerHubAuth;
+  if (!auth) {
+    const dockerConfig = path.join(os.homedir(), '.docker/config.json');
+    if (fs.existsSync(dockerConfig)) {
+      const config = require(dockerConfig);
+      auth = config.auths[addr].auth;
+    }
+  }
+
+  if (auth) {
     const authBuf = new Buffer(auth, 'base64').toString('utf8');
     const [user, pass] = authBuf.split(':');
     return {
