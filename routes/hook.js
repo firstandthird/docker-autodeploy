f8e2aa237b3ecbf2865ab180b870b147e173d96b
Greg Allen <greg@firstandthird.com>
2017-12-09 15:23:12 -0800

    use docker-service-update

--- routes/hook.js
+++ routes/hook.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const Boom = require('boom');
 const Docker = require('dockerode');
 const aug = require('aug');
+const serviceUpdate = require('docker-service-update');
 exports.hook = {
   path: '/',
   method: 'POST',
@@ -13,6 +14,7 @@ exports.hook = {
       },
       payload: {
         name: Joi.string(),
+        clone: Joi.string(),
         image: Joi.string(),
         environment: Joi.object(),
         labels: Joi.object()
@@ -39,63 +41,23 @@ exports.hook = {
       payload(request, done) {
         done(null, request.payload);
       },
-      service(docker, payload, done) {
-        const service = docker.getService(payload.name);
-        done(null, service);
-      },
-      serviceTask(service, done) {
-        service.inspect(done);
-      },
-      taskSpec(settings, server, serviceTask, payload, done) {
-        const task = {
-          version: parseInt(serviceTask.Version.Index, 10),
-          TaskTemplate: {
-            ContainerSpec: {},
-            ForceUpdate: 1,
-          }
-        };
-        if (payload.image) {
-          task.TaskTemplate.ContainerSpec.Image = payload.image;
-        }
-        if (payload.labels) {
-          task.Labels = payload.labels;
-        }
+      async update(auth, docker, payload) {
         let specEnv = {};
-        if (serviceTask.Spec.TaskTemplate.ContainerSpec.Env) {
-          specEnv = server.methods.arrToObj(serviceTask.Spec.TaskTemplate.ContainerSpec.Env);
-        }
-
         if (payload.environment) {
           specEnv = Object.assign({}, specEnv, payload.environment);
         }
-
         specEnv['UPDATED'] = new Date().getTime();
 
-        serviceTask.Spec.TaskTemplate.ContainerSpec.Env = server.methods.objToArr(specEnv);
-
-        const newTask = aug(serviceTask.Spec, task);
-        if (settings.debug) {
-          server.log(['hook', 'debug'], newTask);
-        }
-        done(null, newTask);
-      },
-      pull(auth, payload, server, settings, docker, done) {
-        if (!payload.image) {
-          return done();
-        }
-        const image = payload.image;
-        docker.pull(image, { authconfig: auth }, (err, stream) => {
-          if (err) {
-            return done(err);
-          }
-          docker.modem.followProgress(stream, done);
-          if (settings.debug) {
-            server.log(['swarm', 'pull', 'debug'], image);
-          }
-        });
-      },
-      update(pull, taskSpec, service, auth, done) {
-        service.update(auth, taskSpec, done);
+        const res = await serviceUpdate({
+          docker,
+          auth,
+          serviceName: payload.name,
+          fromService: payload.clone,
+          image: payload.image,
+          labels: payload.labels,
+          environment: specEnv
+        })
+        return res;
       },
       reply(server, update, payload, done) {
         let message = `Updating ${payload.name}`;
