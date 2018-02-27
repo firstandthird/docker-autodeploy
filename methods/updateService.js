848059baacb9815eca5bf3104ddd781e2fa756df
Greg Allen <greg@firstandthird.com>
2018-02-27 11:31:57 -0800

    added UPDATED env var

--- methods/updateService.js
+++ methods/updateService.js
@@ -1,7 +1,12 @@
 module.exports = async function(services, name, url, payload) {
   const server = this;
   try {
-    await services.adjust(name, { force: true });
+    await services.adjust(name, {
+      force: true,
+      env: {
+        UPDATED: new Date().getTime()
+      }
+    });
     server.log([name, 'update', 'success'], {
       message: `${name} updated`,
       url,
